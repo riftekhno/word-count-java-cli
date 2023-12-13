4f1202deaa75bb6b84adce175d682b2fd91670d4
Riftekhno <riftekhno@gmail.com>
2023-12-13 09:05:17 +0800

    Added count words code generated by chatgpt

--- src/main/java/com/rifdi/cli/App.java
+++ src/main/java/com/rifdi/cli/App.java
@@ -1,38 +1,57 @@
 package com.rifdi.cli;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )  throws Exception
-    {
-        System.out.println( "Hello World!" );
-        File directory = new File("./");
-        System.out.println(directory.getAbsolutePath());
+public class App {
 
+    public static void main(String[] args) {
+        String fileName = "./files/example1.txt"; // Replace with your file path
         try {
-            int count =0;
-            File file = new File("./files/example1.txt");
-            FileInputStream fis = new FileInputStream(file);
-            byte[] bytesArray = new byte[(int)file.length()];
-            fis.read(bytesArray);
-            String s = new String(bytesArray);
-            String [] data = s.split(" ");
-            for (int i=0; i<data.length; i++) {
-                count++;
-            }
-            System.out.println("Number of characters in the given file are " +count);
+            Map<String, Integer> wordCountMap = countWords(fileName);
+            displaySortedWordCounts(wordCountMap);
         } catch (IOException e) {
             e.printStackTrace();
-            // handle exception correctly.
         }
     }
+
+    private static Map<String, Integer> countWords(String fileName) throws IOException {
+        Map<String, Integer> wordCountMap = new HashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Split the line into words using space as a delimiter
+                String[] words = line.split("\\s+");
+
+                // Count the occurrences of each word
+                for (String word : words) {
+                    // Remove punctuation and convert to lowercase for better matching
+                    word = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
+
+                    if (!word.isEmpty()) {
+                        wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+                    }
+                }
+            }
+        }
+
+        return wordCountMap;
+    }
+
+    private static void displaySortedWordCounts(Map<String, Integer> wordCountMap) {
+        wordCountMap.entrySet()
+                .stream()
+                .sorted((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()))
+                .forEach(entry ->
+                        System.out.println(entry.getKey() + ": " + entry.getValue())
+                );
+    }
 }
+
 
 
 
